1c85280f4708c30e24983b562d48c2d9a5106a3f
yozibozi03 <43601211+yozibozi03@users.noreply.github.com>
2018-10-03 11:34:00 +0300

    add  files
    dobavih dialog

---
+++ dialog.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"Kak se kazvash?";
+    char ime [15];
+    cin>>ime;
+    cout<<"Zdravei"<<ime<<"!"<<endl;
+    cout<<"Na kolko si godini?";
+    char godini [20];
+    cin>>godini;
+    return 0;
+}
